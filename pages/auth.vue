b184e3f8b730412bf319d5c81ed181fb3ea2ffeb
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-04-06 05:36:35 +0400

    fix: use tabs to switch login/signup

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,54 +1,33 @@
 <template>
   <v-app id="auth">
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-tabs
+              v-model="tab"
+              :grow="true"
+            >
+              <v-tab> Login </v-tab>
+              <v-tab> Signup </v-tab>
+            </v-tabs>
+
             <v-card>
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>Login form</v-toolbar-title>
-              </v-toolbar>
-
               <v-card-text>
-                <user-login-form
-                  @submit="login"
-                />
-              </v-card-text>
-            </v-card>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
-            <v-card>
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>Signup form</v-toolbar-title>
-              </v-toolbar>
-
-              <v-card-text>
-                <user-signup-form
-                  @submit="signup"
-                />
+                <v-tabs-items v-model="tab">
+                  <v-tab-item
+                    :transition="false"
+                    :reverse-transition="false"
+                  >
+                    <user-login-form @submit="login"/>
+                  </v-tab-item>
+                  <v-tab-item
+                    :transition="false"
+                    :reverse-transition="false"
+                  >
+                    <user-signup-form @submit="signup" />
+                  </v-tab-item>
+                </v-tabs-items>
               </v-card-text>
             </v-card>
           </v-col>
@@ -61,6 +40,10 @@
 <script>
 import UserLoginForm from '@/components/UserLoginForm'
 import UserSignupForm from '@/components/UserSignupForm'
+
+const data = () => ({
+  tab: null
+})
 
 const methods = {
   login (formData) {
@@ -77,6 +60,7 @@ export default {
     UserLoginForm,
     UserSignupForm
   },
+  data,
   methods
 }
 </script>
