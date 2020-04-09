757d97f58f60bc064f523df7238024f58a77df91
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-04-09 09:14:32 +0400

    fix: add app drawer w/ user info and nav

---
+++ client/components/TheDrawerContent/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-list dense>
+    <v-list-item two-line>
+      <v-list-item-avatar color="primary">
+        <v-icon>
+          mdi-account-circle
+        </v-icon>
+      </v-list-item-avatar>
+
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ user.name }}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{ user.email }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider />
+
+    <v-list-item
+      v-for="(item, i) in navItems"
+      :key="i"
+      :to="item.to"
+      router
+      exact
+    >
+      <v-list-item-action>
+        <v-icon>
+          {{ item.icon }}
+        </v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.title" />
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+const props = {
+  user: {
+    type: Object,
+    required: true
+  },
+  navItems: {
+    type: Array,
+    default: () => []
+  }
+}
+
+export default {
+  name: 'TheDrawerContent',
+  props
+}
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,6 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar fixed app>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+      dark
+    >
+      <the-drawer-content
+        :user="user"
+        :nav-items="items"
+      />
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn to="/auth/logout" nuxt block color="error">
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="true" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
       <v-toolbar-title v-text="title" />
     </v-app-bar>
 
@@ -9,19 +32,43 @@
         <nuxt />
       </v-container>
     </v-content>
-
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      title: 'Tuck'
+import { mapGetters } from 'vuex'
+
+import TheDrawerContent from '@/components/TheDrawerContent'
+
+//
+
+const computed = {
+  ...mapGetters(['user'])
+}
+
+const data = () => ({
+  drawer: false,
+  items: [
+    {
+      icon: 'mdi-home',
+      title: 'Welcome',
+      to: '/',
+      isExact: true
+    },
+    {
+      icon: 'mdi-home-analytics',
+      title: 'Dashboard',
+      to: '/dashboard'
     }
-  }
+  ],
+  title: 'Tuck'
+})
+
+export default {
+  components: {
+    TheDrawerContent
+  },
+  data,
+  computed
 }
 </script>
